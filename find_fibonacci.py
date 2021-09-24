bd251777af063e12ca63fd0408302e80814e1316
hidayat <hidayat.hidayat@accenture.com>
2021-09-24 20:22:50 +0700

    feat: Add new file find_fibonacci.py

---
+++ find_fibonacci.py
@@ -0,0 +1,15 @@
+def find_fibonacci(x: int) -> bool:
+    """
+    Menemukan bilangan bulat x di dalam suatu deret fibonacci.
+    Apabila x ada di dalam suatu deret fibonacci, maka kembalikan True.
+    Jika tidak ada, maka kembalikan False
+    """
+    # write your code here
+
+
+if __name__ == "__main__":
+    """Jalankan beberapa test-case di bawah sini
+    """
+    print(find_fibonacci(1))
+    print(find_fibonacci(10))
+    print(find_fibonacci(11))
