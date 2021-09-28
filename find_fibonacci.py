b476fdca342aec9ebf34b662858f7bb2a06d25dc
shagnaz <shagnazintania@gmail.com>
2021-09-29 00:10:14 +0700

    Update fibonacci function

--- find_fibonacci.py
+++ find_fibonacci.py
@@ -1,3 +1,4 @@
+list = []
 def find_fibonacci(x: int) -> bool:
     """
     Menemukan bilangan bulat x di dalam suatu deret fibonacci.
@@ -5,7 +6,18 @@ def find_fibonacci(x: int) -> bool:
     Jika tidak ada, maka kembalikan False
     """
     # write your code here
-
+    f0 = 0 #seed fibonacci
+    f1 = 1 #seed fibonacci
+    list.extend([f0,f1])
+    for idx, value in enumerate(list):
+        if value <= x:
+            fibo = value + list[idx+1]
+            list.append(fibo)
+   
+    if (x in list):
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     """Jalankan beberapa test-case di bawah sini
